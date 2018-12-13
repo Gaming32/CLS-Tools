a59078e4b5a82b0bfc0cde5480897575cdcd9f6d
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2018-12-13 10:48:09 -0600

    Update CLS_Tools.Tools

--- CLS Tools/Tools.cs
+++ CLS Tools/Tools.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CLS_Tools
 {
     class Tools
     {
+        DateTime startTime = DateTime.Now;
+
+        public TimeSpan runLength { get
+            {
+                return DateTime.Now - startTime;
+            } }
+
+        public void Log(string message, string path = "", bool includeAppName = false)
+        {
+            if (!File.Exists(path))
+                path = Directory.GetCurrentDirectory();
+            if (includeAppName)
+                path += System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            path += startTime.ToString("MM-dd-yyyy-HH-mm-ss");
+            path += ".log.csv";
+
+            StreamWriter logger = new StreamWriter(path);
+            if (logger.BaseStream.Length == 0)
+                logger.WriteLine("TIMESTAMP,MESSAGE");
+            logger.WriteLine(runLength.TotalSeconds.ToString() + "," + message);
+        }
     }
 }
